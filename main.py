6a68ce56f819120bb0eb2a2c068ef13400d9139a
Adam <AdamEaton76@gmail.com>
2018-06-23 15:14:57 +0100

    Testing Switching LEDs based on distance sensors

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import time
 #TRIG = 23
 #ECHO = 24
 
-def calc_distance(TRIG, ECHO):
-    GPIO.setmode(GPIO.BCM)
-
+def calc_distance(TRIG, ECHO):  
     GPIO.setup(TRIG, GPIO.OUT)
     GPIO.setup(ECHO, GPIO.IN)
 
@@ -28,10 +26,33 @@ def calc_distance(TRIG, ECHO):
     distance = pulse_duration * 17150
 
     distance = round(distance, 2)
-    GPIO.cleanup()
-
     return distance
 
+def LED_On():
+    GPIO.setup(20, GPIO.OUT)
+    GPIO.setup(21, GPIO.OUT)
+
+    GPIO.output(20, True)
+    GPIO.output(21, True)
+
+def LED_Off():
+    GPIO.output(20, False)
+    GPIO.output(21, False)
+
 while True:
+    GPIO.setmode(GPIO.BCM)
+
+    Sensor_One = calc_distance(23, 24)
+    Sensor_Two = calc_distance(17, 27)
+    LED_Switch = False
+
+    if Sensor_One <= 7.00 or Sensor_Two <= 7.00:
+        LED_Switch = True
+
+    if LED_Switch:
+        LED_On()
+    else:
+        LED_Off()
+
     print("Distance Sensor One: ", calc_distance(23, 24), "cm")
     print("Distance Sensor Two: ", calc_distance(17, 27), "cm")
