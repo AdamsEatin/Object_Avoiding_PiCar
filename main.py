47ee26b780c21c7efba6f7f9f23e33b61eec9693
Adam <AdamEaton76@gmail.com>
2018-06-25 13:35:03 +0100

    Testing l298N IN pins for motor control

--- main.py
+++ main.py
@@ -34,23 +34,43 @@ while True:
     L_ECHO = 13
     R_TRIG = 20
     R_ECHO = 21
+    
     LED1 = 19
     LED2 = 26
 
-    # Setting up LED Pins
+    IN1 = 18
+    IN2 = 17
+    IN3 = 27
+    IN4 = 22
+
+    # Setting up Pins
     GPIO.setup(LED1, GPIO.OUT)
     GPIO.setup(LED2, GPIO.OUT)
 
+    GPIO.setup(IN1, GPIO.OUT)
+    GPIO.setup(IN2, GPIO.OUT)
+    GPIO.setup(IN3, GPIO.OUT)
+    GPIO.setup(IN4, GPIO.OUT)
+
+
+    GPIO.output(IN1, True)
+    time.sleep(3)
+    GPIO.output(IN1, False)
+
+    """
     #TODO : Rename to represent Front Left/Right, Back Left/Right Sensors
     Sensor_One = calc_distance(L_TRIG, L_ECHO)
     Sensor_Two = calc_distance(R_TRIG, R_ECHO)
 
+    
     if Sensor_One <= 9.00 or Sensor_Two <= 9.00:
         GPIO.output(LED1, True)
         GPIO.output(LED2, True)
+
     else:
         GPIO.output(LED1, False)
         GPIO.output(LED2, False)
 
     print("Distance Sensor One: ", Sensor_One, "cm")
     print("Distance Sensor Two: ", Sensor_Two, "cm")
+    """
